22523d90c1ab37c24b7f74eb3eef7972d544628b
moka <gaoyuanfell@sina.com>
2020-01-19 19:12:11 +0800

    2020-01-19 19:12:11 → moka:gaoyuanfell@sina.com | msg →

---
+++ web/build/precache-manifest.75b662d9a6724b6bdab9642e28cbea1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26494949bdc4af6e6c1b2a0d1999afe9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2d29fe32832ff06390f",
+    "url": "/static/css/main.f62804d4.chunk.css"
+  },
+  {
+    "revision": "007d16eb13298cbd7191",
+    "url": "/static/js/2.eef081e3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.eef081e3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d2d29fe32832ff06390f",
+    "url": "/static/js/main.99a1c320.chunk.js"
+  },
+  {
+    "revision": "c9d9b755392b2ab37447",
+    "url": "/static/js/runtime-main.e4135d32.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
